b3e58562580fc517cb9402f958e60ab7bd142aa2
Imaad Uddin <imaaduddin27@gmail.com>
2024-08-29 01:18:10 -0500

    building board

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 
 // function App() {
 //   return (
@@ -25,6 +25,28 @@
 // export default App;
 
 // My Code
-export default function Square() {
-  return <button className="square">X</button>
+function Square({value}) {
+  return <button className="square">{value}</button>
+}
+
+export default function Board() {
+  return (
+    <>
+      <div className='board-row'>
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+      <div className='board-row'>
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+      </div>
+      <div className='board-row'>
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
+    </>
+  );
 }
